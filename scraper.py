3066ea59742c4ad6c85516d5626e9c62c9078f49
krynens <75901402+krynens@users.noreply.github.com>
2021-03-07 00:27:46 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,31 +8,27 @@ import scraperwiki
 
 today = datetime.today()
 
-for i in range(1, 50):
-    try:
-        print(f'Getting page {i}')
-        url = 'https://www.melbourne.vic.gov.au/building-and-development/property-information/planning-building-registers/Pages/town-planning-permits-register-search-results.aspx?AdvertisingOnly=on&page=' + str(i)
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content, 'lxml')
+url = 'http://ecouncil.lanecove.nsw.gov.au/trim/advertisedDAs.aspx'
+r = requests.get(url)
+soup = BeautifulSoup(r.content, 'lxml')
 
-        table = soup.find('tbody')
-        rows = table.find_all('tr', class_='detail')
+table = soup.find('div', class_='bodypanel')
+rows = table.find_all('table', class_='tabular-data table')
 
-        for row in rows:
-            record = {}
-            record['address'] = row.find('td', class_='column2').text
-            date_received_raw = row.find('td', class_='column3').text
-            record['date_received'] = datetime.strptime(
-                date_received_raw, "%d/%m/%Y").strftime("%Y-%m-%d")
-            record['date_scraped'] = today.strftime("%Y-%m-%d")
-            record['description'] = row.find('td', class_='column4').text
-            record['council_reference'] = row.find('td', class_='column1').text
-            record['info_url'] = 'https://www.melbourne.vic.gov.au' + \
-                str(row.find('td', class_='column1')).split('"')[5]
+for row in rows:
+    record = {}
+    address = row.find('td', class_='current-development-applications')
+    suburb = address.find_previous('h2').text.strip().title()
+    record['address'] = f'{address.text.strip()}, {suburb}'
+    record['date_scraped'] = today.strftime("%Y-%m-%d")
+    record['description'] = row.find('td', colspan='2').text.strip()
+    record['council_reference'] = row.find('a', style='text-decoration:underline; color:blue').text
+    record['info_url'] = str(row.find(
+        'a', style='text-decoration:underline; color:blue')).split('"')[1]
+    record['on_notice_from'] = row.find_all('td')[13].text.strip().split(' Expiry: ')[0]
+    record['on_notice_to'] = row.find_all('td')[13].text.strip().split(' Expiry: ')[1]
 
-            scraperwiki.sqlite.save(
-                unique_keys=['council_reference'], data=record, table_name="data")
+    print(record)
 
-    except:
-        print('Scraper finished.')
-        break
+    scraperwiki.sqlite.save(
+        unique_keys=['council_reference'], data=record, table_name="data")
